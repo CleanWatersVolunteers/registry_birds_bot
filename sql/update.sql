80b2647705759a4fdcfefc39d6497af5732b25ab
elagin <elagin.pasha@gmail.com>
2025-01-31 01:01:54 +0300

    Исправить записи в таблице manipulations #107

--- sql/update.sql
+++ sql/update.sql
@@ -1,9 +1,7 @@
-INSERT INTO `places` (`id`, `name`) VALUES
-	(5, 'Нянька');
+-- Лишний столбец animals.female #102
+alter table `animals` drop COLUMN `female`;
 
-INSERT INTO `manipulations` (`id`, `name`, `place_list`) VALUES
- (7, 'Взвешивание', '2,5');
-
-INSERT INTO `arms` (`id`, `place_id`, `location_id`) VALUES
-(8, 5, 0),
-(9, 5, 1);
+-- Исправить записи в таблице manipulations #107
+UPDATE `manipulations` SET `place_list`='3,5' WHERE `name`='Бриллиантовые глаза';
+UPDATE `manipulations` SET `place_list`='3,5' WHERE `name`='Энтерофурил + физ.раствор 20мл в рот';
+UPDATE `manipulations` SET `place_list`='3' WHERE  `name`='Рингер 50/50 + Глюкоза 10мл';
