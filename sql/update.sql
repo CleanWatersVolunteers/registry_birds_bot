266f67c578ff3baa06c7652a10f7a466d358a5c6
elagin <elagin.pasha@gmail.com>
2025-01-31 01:01:54 +0300

    Исправить записи в таблице manipulations #107

--- sql/update.sql
+++ sql/update.sql
@@ -4,4 +4,3 @@ alter table `animals` drop COLUMN `female`;
 -- Исправить записи в таблице manipulations #107
 UPDATE `manipulations` SET `place_list`='3,5' WHERE `name`='Бриллиантовые глаза';
 UPDATE `manipulations` SET `place_list`='3,5' WHERE `name`='Энтерофурил + физ.раствор 20мл в рот';
-UPDATE `manipulations` SET `place_list`='3' WHERE  `name`='Рингер 50/50 + Глюкоза 10мл';
