5d1231e3adf09a9a57772656c7b895f1aaaf769e
ddubovka <ddubovka@ozon.ru>
2025-01-31 14:24:42 +0300

    Добавить АРМ "Нянька" FIX #80

--- sql/update.sql
+++ sql/update.sql
@@ -4,3 +4,6 @@ alter table `animals` drop COLUMN `female`;
 -- Исправить записи в таблице manipulations #107
 UPDATE `manipulations` SET `place_list`='3,5' WHERE `name`='Бриллиантовые глаза';
 UPDATE `manipulations` SET `place_list`='3,5' WHERE `name`='Энтерофурил + физ.раствор 20мл в рот';
+
+INSERT INTO `manipulations` values
+(7, 'Взвешивание глаза', '2,5');
