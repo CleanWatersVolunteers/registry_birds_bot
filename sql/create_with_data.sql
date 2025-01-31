29e7563a44414c122c2acb6f8cf17484e4ae5dbc
ddubovka <ddubovka@ozon.ru>
2025-01-31 16:17:27 +0300

    Добавить АРМ "Нянька" FIX #80

--- sql/create_with_data.sql
+++ sql/create_with_data.sql
@@ -44,7 +44,9 @@ INSERT INTO `arms` (`id`, `place_id`, `location_id`) VALUES
 	(4, 4, 0),
 	(5, 0, 1),
 	(6, 1, 1),
-	(7, 2, 2);
+	(7, 2, 2),
+	(8, 5, 0),
+	(9, 5, 1);
 
 -- Дамп структуры для таблица registry_birds.history
 CREATE TABLE IF NOT EXISTS `history` (
@@ -86,7 +88,8 @@ INSERT INTO `manipulations` (`id`, `name`, `place_list`) VALUES
 	(3, 'Отказ от питья', '5'),
 	(4, 'Энтерофурил + физ.раствор 20мл в рот', '3,5'),
 	(5, 'Рингер 50/50 + Глюкоза 10мл', '3'),
-	(6, 'Бриллиантовые глаза', '3,5');
+	(6, 'Бриллиантовые глаза', '3,5'),
+	(7, 'Взвешивание', '5');
 
 -- Дамп структуры для таблица registry_birds.numerical_history
 CREATE TABLE IF NOT EXISTS `numerical_history` (
@@ -126,7 +129,8 @@ INSERT INTO `places` (`id`, `name`) VALUES
 	(1, 'Первичка на мойке'),
 	(2, 'Прием в стационар'),
 	(3, 'Первичка в стационаре'),
-	(4, 'Медицинский прием');
+	(4, 'Медицинский прием'),
+	(5, 'Нянька');
 
 -- Дамп структуры для таблица registry_birds.place_history
 CREATE TABLE IF NOT EXISTS `place_history` (
