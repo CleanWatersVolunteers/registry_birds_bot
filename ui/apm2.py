9c31c0061e10db660203c37a1c49a9f8b91078b7
elagin <elagin.pasha@gmail.com>
2025-02-10 17:32:31 +0300

    Актуализировать первичку на мойке #151

--- ui/apm2.py
+++ ui/apm2.py
@@ -1,8 +1,13 @@
 # Первичка на мойке
 
 from database import Database as db
+from storage import storage
 
-apm2_text = f"Выполните мойку животного и нажмите 'Готово'"
+apm2_text_animal_header = "Животное №:"
+apm2_text = f"Выполните необходимые манипуляции и нажмите 'Готово'"
+apm2_text_done = 'Готово'
+apm2_text_cancel = 'Отмена'
+
 
 ##################################
 # Global API
@@ -10,18 +15,25 @@ apm2_text = f"Выполните мойку животного и нажмите
 
 def apm2_start(username, text, key=None):
 	user = db.get_user(username)
-	if user["animal_id"] == None:
-		user["animal_id"] = db.get_animal_id(text)	
-		if user["animal_id"] == None:
-			return (
-				f'❌ Животное с номером {text} не найдено!',
-				{"Отмена": "entry_cancel"}, None
-			)
+	animal = storage.get_animal_by_bar_code(text)
+	if animal is not None:
+		user["animal_id"] = animal['animal_id']
+	if user["animal_id"] is None:
+		return (
+			f'❌ Животное с номером {text} не найдено!',
+			{apm2_text_cancel: "entry_cancel"}, None
+		)
 	return (
-		apm2_text,
-		{"Готово": "apm2_done", "Отмена": "entry_cancel"},
+		f'{apm2_text_animal_header}{animal["bar_code"]}\n{apm2_text}',
+		{apm2_text_done: "apm2_done", apm2_text_cancel: "entry_cancel"},
 		None
 	)
 
+
 def apm2_entry(username, text, key):
+	user = db.get_user(username)
+	print(username)
+	# todo Получать arm_id из базы
+	arm_id = 1
+	storage.insert_place_history(arm_id, user["animal_id"], username)
 	return None, None
