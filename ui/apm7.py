85d7f5ff6d33dcb6c7462cba4c6c8dd9056e1371
elagin <elagin.pasha@gmail.com>
2025-02-14 02:00:40 +0300

    Перенести старый функционал в new_ui История #168

--- ui/apm7.py
+++ ui/apm7.py
@@ -1,6 +1,47 @@
 # История
 
-from database import Database as db
+from storage import storage
+
+apm7_text_ok = 'OK'
+apm7_text_animal_number = "Номер животного"
+apm7_text_capture_place = "Место отлова"
+apm7_text_capture_time = "Время отлова"
+apm7_text_pollution_degree = "Степень загрязнения"
+apm7_text_weight = "Вес"
+apm7_text_not_specified = "Не указан"
+apm7_text_species = "Вид"
+apm7_text_clinical_condition = "Клиническое состояние"
+apm7_manipulations_not_found = "Манипуляции не найдены"
+
+apm7_capture_datetime_format = "%d.%m.%y %H:%M"
+apm7_time_format = "%H:%M"
+apm7_date_format = "%d.%m.%y"
+
+
+def add_hdr_item(label, value):
+	text = f'{label}: '
+	if value:
+		text += f'{value}\n'
+	else:
+		text += '-\n'
+	return text
+
+
+def ui_welcome_get_card(animal_id):
+	animal = storage.get_animal_by_id(animal_id)
+	if animal:
+		text = add_hdr_item(apm7_text_animal_number, animal["bar_code"])
+		text += add_hdr_item(apm7_text_capture_place, animal["place_capture"])
+		text += add_hdr_item(apm7_text_capture_time, animal["capture_datetime"].strftime(apm7_capture_datetime_format))
+		text += add_hdr_item(apm7_text_pollution_degree, animal["degree_pollution"])
+		text += add_hdr_item(apm7_text_weight,
+							 f"{animal['weight']} гр." if animal["weight"] else apm7_text_not_specified)
+		text += add_hdr_item(apm7_text_species, animal["species"] if animal["species"] else apm7_text_not_specified)
+		text += add_hdr_item(apm7_text_clinical_condition, animal["clinical_condition_admission"] if animal[
+			"clinical_condition_admission"] else apm7_text_not_specified)
+		text += '---------------\n'
+		return text
+	return None
 
 
 ##################################
@@ -8,32 +49,42 @@ from database import Database as db
 ##################################
 
 def apm7_start(username, text, key=None):
-	user = db.get_user(username)
-	if user["animal_id"] == None:
-		user["animal_id"] = db.get_animal_id(text)	
-		if user["animal_id"] == None:
-			return (
-				f'❌ Животное с номером {text} не найдено!',
-				{"Отмена": "entry_cancel"}, None
-			)
-
-	history = db.get_history(user["animal_id"])
-	if len(history) == 0:
+	animal = storage.get_animal_by_bar_code(text)
+	if animal is None:
 		return (
-			"❌ Манипуляции не найдены!\n",
-			{"Готово": "entry_cancel"}, None
+			f'❌ Животное с номером {text} не найдено!',
+			{apm7_text_ok: "entry_cancel"},
+			None
 		)
-	date = history[0]["datetime"].split(' ')[0]
-	text = f'{date}:\n'
-	for item in history:
-		curr_date = item["datetime"].split(' ')[0]
-		curr_time = item["datetime"].split(' ')[1]
-		if curr_date != date:
-			date = curr_date
-			text += f'{date}:\n'
-		text += f'{curr_time} - {item["manipulation_id"]} - {item["tg_nickname"]}\n'
+
+	text = ui_welcome_get_card(animal["animal_id"])
+	numerical_history = storage.get_animal_numerical_history(animal["animal_id"])
+	history = storage.get_animal_history(animal["animal_id"])
+	place_history = storage.get_place_history(animal["animal_id"])
+
+	combined_history = numerical_history + history + place_history
+	sorted_history = sorted(combined_history, key=lambda item: item['datetime'])
+	result_string = ""
+	current_date = None
+	for item in sorted_history:
+		formatted_date = item['datetime'].strftime(apm7_date_format)
+		if current_date != formatted_date:
+			if current_date is not None:
+				result_string += "\n"
+			result_string += f"{formatted_date}\n"
+			current_date = formatted_date
+		if 'type_name' in item:  # Элемент из numerical_history
+			result_string += f"{item['datetime'].strftime(apm7_time_format)} - {item['type_name']}: {item['value']} {item['type_units']} - {item['tg_nickname']}\n"
+		elif 'manipulation_name' in item:  # Элемент из history
+			result_string += f"{item['datetime'].strftime(apm7_time_format)} - {item['manipulation_name']} - {item['tg_nickname']}\n"
+		else:
+			result_string += f"{item['datetime'].strftime(apm7_time_format)} - {item['place_name']} - {item['location_name']}\n"
+	if result_string == "":
+		text += apm7_manipulations_not_found
+	else:
+		text += result_string.strip()
 	return text, {"Готово": "entry_cancel"}, None
-	
+
 
 def apm7_entry(username, text, key):
 	return None, None
